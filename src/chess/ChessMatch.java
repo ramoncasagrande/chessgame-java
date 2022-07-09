671a3d757e42db624c1c67fca9de71d2c449adae
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-07-09 19:58:07 -0300

    BoardException and Defensive Programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,7 +27,6 @@ public class ChessMatch {
 	
 	private void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		board.placePiece(new Rook(board, Color.WHITE), new Position(6,7));
 		board.placePiece(new King(board,Color.BLACK), new Position(5,5));
 	}
 
